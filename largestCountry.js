df3dc33aafc123f6cc1c60874b3c6dd819cde9f6
baodai1207 <dai.huynh@ucdenver.edu>
2022-04-26 14:34:19 -0600

    finished

--- largestCountry.js
+++ largestCountry.js
@@ -29,16 +29,35 @@ const largestCountry = (countries, cities, populations) => {
   const country_list = countries.map((country) => {
     const city_list = cities.filter((city) => city.country_id === country.id);
     // console.log(city_list);
-    city_list.map((city) => {
-      const total_pop = populations.reduce((total, pop) =>
-        pop.city_id === city.id ? total + pop.amount : total
-      );
-      console.log(city);
-      console.log(total_pop);
-    });
-    console.log(populations);
+    let total = 0;
+    for (let i = 0; i < city_list.length; i++) {
+      for (let j = 0; j < populations.length; j++) {
+        if (populations[j].city_id === city_list[i].id) {
+          total += populations[j].amount;
+          // console.log(total);
+          // console.log(country.name);
+        }
+      }
+    }
+    return {
+      name: country.name,
+      populations: total,
+    };
+
+    // city_list.map((city) => {
+    //   const total_pop = populations.reduce((total, pop) =>
+    //     pop.city_id === city.id ? total + pop.amount : total
+    //   );
+    //   console.log(city);
+    //   console.log(total_pop);
+    // });
+    // console.log(populations);
   });
-  return { name: 'ToBeDecided', population: 0 };
+  const largest_pop = country_list.reduce((largest, country) =>
+    (largest.populations || 0) > country.populations ? largest : country
+  );
+
+  return largest_pop;
 };
 
 module.exports = largestCountry;
